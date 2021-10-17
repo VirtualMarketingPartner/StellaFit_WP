82d985db8cf50c53f5d88721d85f98dbac80d1e1
Taylor Desens <hello@taylordesens.com>
2021-10-17 11:26:58 -0500

    Update slider

--- partials/flex/slide_flex.php
+++ partials/flex/slide_flex.php
@@ -1,16 +1,34 @@
 
-<?php $type = get_sub_field('type'); ?>
+<?php if( have_rows('slideshow')): ?>
 
-<section class="container-fluid slide_flex" >
-	<?php if( $type = 'quote'): ?>
-		<?php if(have_rows('quotes')): while(have_rows('quotes')): ?>
-			<p>Quotation</p>
-		<?php endwhile; endif; ?>
-
-	<?php elseif( $type = 'traditional' ): ?>
-
-		<?php if(have_rows('slideshow')): while(have_rows('slideshow')): ?>
-			<p>Traditional</p>
-		<?php endwhile; endif; ?>
-	<?php endif; ?>
-</section><!-- .slide_flex -->
+	<section class="container-fluid slide_flex" >
+		<div id="slideshow" class="carousel slide" data-ride="carousel" >
+			<ol class="carousel-indicators" >
+				<?php while( have_rows('slideshow')): $count = get_row_index(); the_row(); ?>
+				<li data-target="#slideshow" data-slide-to="<?php echo $count; ?>" class="<?php if($count==0){echo 'active'; } ?>" ></li>
+				<?php endwhile; ?>
+			</ol>
+			<div class="carousel-inner" >
+			<?php while( have_rows('slideshow')):  $count = get_row_index();  the_row(); ?>
+				<div class="carousel-item <?php if( $count== 0 ){echo 'active'; } ?>" style="background-color:var(--white);" >
+					<div class="container" >
+						<div class="row" >
+                            <div class="col-12 col-md-3" >
+                                <div class="image-wrapper" style="background-image:url(<?php the_sub_field('image'); ?>);" ></div>
+                            </div>
+							<div class="col-12 col-md-9 lg-offset-1 col-lg-8" >
+								<div class="hero-text animate" >
+									<div class="content" >
+										<?php the_sub_field('content'); ?>
+									</div><!-- .content -->
+								</div><!-- .hero-text -->
+							</div><!-- .col -->
+						</div><!-- .row -->
+					</div><!-- .container -->
+					
+				</div><!-- .carousel-item -->
+			<?php endwhile; ?>		
+			</div><!-- .carousel-inner -->
+		</div><!-- #slideshow -->
+	</section><!-- .slide_flex -->
+<?php endif; ?>
