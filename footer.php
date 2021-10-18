8508238ddb6aee6eff3b88898e17499d43a8056a
Obie Silva <obiesilva@gmail.com>
2021-10-18 08:20:35 -0400

    Minor footer style updates

--- footer.php
+++ footer.php
@@ -1,36 +1,37 @@
-	</div><!-- #content -->
+</div><!-- #content -->
 
-	<div id="footer">
-		<div class="container-fluid" id="footer_row1">
-			<div class="container">
-				<div class="row">
-					<div class="col-12 col-md-4 text-center text-md-left">
-						<a class="image-wrapper" id="footer-logo" href="<?php bloginfo('url'); ?>" style="background-image:url(<?php the_field('logo', 'options'); ?>);"></a>
-						<?php the_field('footer_address', 'options'); ?>
-					</div><!-- .col -->
+<div id="footer">
+	<div class="container-fluid" id="footer_row1">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 col-md-4 text-center text-md-left">
+					<a class="image-wrapper" id="footer-logo" href="<?php bloginfo('url'); ?>" style="background-image:url(<?php the_field('logo', 'options'); ?>);"></a>
+					<?php the_field('footer_address', 'options'); ?>
+				</div><!-- .col -->
 
-					<div class="col-12 col-md-4 text-center text-md-left">
-						<?php the_field('footer_phone', 'options'); ?>
-						<?php get_template_part('partials/socials'); ?>
-					</div><!-- .col -->
+				<div class="col-12 col-md-4 text-center text-md-left">
+					<?php the_field('footer_phone', 'options'); ?>
+					<?php get_template_part('partials/socials'); ?>
+				</div><!-- .col -->
 
-					<div class="col-12 col-md-3 text-center text-md-left">
-						<h5>Menu</h5>
-						<?php wp_nav_menu(array('theme_location' => 'primary')); ?>
-					</div><!-- .col -->
-				</div><!-- .row -->
-				<hr>
-				<div class="row">
-					<div class="col-12 text-center copyright">
-						<?php the_field('copyright', 'options'); ?>
-					</div><!-- .col -->
-				</div>
-			</div><!-- .container -->
-		</div><!-- #footer_row1 -->
-	</div><!-- #footer -->
+				<div class="col-12 col-md-3 text-center text-md-left">
+					<h6>Menu</h6>
+					<?php wp_nav_menu(array('theme_location' => 'primary')); ?>
+				</div><!-- .col -->
+			</div><!-- .row -->
+			<hr>
+			<div class="row">
+				<div class="col-12 text-center copyright">
+					<?php the_field('copyright', 'options'); ?>
+				</div><!-- .col -->
+			</div>
+		</div><!-- .container -->
+	</div><!-- #footer_row1 -->
+</div><!-- #footer -->
 
-	<?php wp_footer(); ?>
+<?php wp_footer(); ?>
 
-	</body>
+</body>
+<link rel="preload" href="/library/fonts/SeptemberMornings.woff2" as="font" type="font/woff2" crossorigin>
 
-	</html>
+</html>
