5c438b9b5fba284057e62348837c51ee4209bb50
Taylor Desens <hello@taylordesens.com>
2021-11-02 19:53:39 -0500

    Minor style improvements to the circle images

--- partials/flex/combo_flex.php
+++ partials/flex/combo_flex.php
@@ -1,13 +1,15 @@
 <?php
 	$contentCol = 8; //get_sub_field('content_width'); 
 	$imageCol = 4; //get_sub_field('image_width'); 
+	$image = get_sub_field('image');
 	$imageSize =  cover; //get_sub_field('image_cover');
+	$imagePos = get_sub_field('image_position');
 ?>
 <div class="container combo_flex" >
 	<div class="row vcenter" >
 		<?php if( get_sub_field('layout') == 'image-left' ): ?>
 		<div class="col-12 col-md-<?php echo $imageCol; ?>" >
-			<div class="image-wrapper animate" style="background-image:url(<?php the_sub_field('image'); ?>); background-size:<?php echo $imageSize; ?>" ></div>
+			<div class="image-wrapper animate" style="background-image:url(<?php echo $image; ?>); background-size:<?php echo $imageSize; ?>; background-position:<?php echo $imagePos; ?>" ></div>
 		</div><!-- .col -->
 		<div class="col-12 col-md-<?php echo $contentCol; ?>" >
 			<div class="text-wrapper animate slow" >
@@ -22,7 +24,7 @@
 			</div><!-- .text-wrapper -->
 		</div><!-- .col -->
 		<div class="col-12 col-md-<?php echo $imageCol; ?>" >
-			<div class="image-wrapper animate" style="background-image:url(<?php the_sub_field('image'); ?>); background-size:<?php echo $imageSize; ?>" ></div>
+		<div class="image-wrapper animate" style="background-image:url(<?php echo $image; ?>); background-size:<?php echo $imageSize; ?>; background-position:<?php echo $imagePos; ?>" ></div>
 		</div><!-- .col -->
 		<?php endif; ?>
 	</div><!-- .row -->
